9f3dbeb494e8b7961ff046e591660f7464f8fb0e
Rubén García Pedrejón <62309228+rubengp39@users.noreply.github.com>
2021-08-23 13:37:44 +0200

    Create 1549A_GregorAndCryptography.cpp

---
+++ A-Set/1549A_GregorAndCryptography.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+using namespace std;
+typedef long long ll;
+#define pb push_back
+const int INFINITE = 10000000;
+
+void solve()
+{
+	int n, a, b; cin >> n;
+	
+	a = 2;
+	b = n -1;
+
+	cout << a << " " << b << '\n';
+}
+
+int main()
+{	
+	int t;
+	cin >> t;
+	while(t--) 
+		solve();
+
+return 0;
+}
